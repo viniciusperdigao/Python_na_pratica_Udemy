06a67c9bb08d9006d56421db4d338781a3da698f
Vinicius Perdigao <eng.viniciusperdigao@gmail.com>
2023-01-13 20:58:20 -0300

    Exercício 2

---
+++ Exercício 2.py
@@ -0,0 +1,7 @@
+# Faça um programa que solicite um número do usuário e apresente a seguinte mensagem na tela:
+
+# "O número digitado foi [número]".
+
+numero = float(input('Digite um número: '))
+
+print(f'O número digitado foi {numero}.')
