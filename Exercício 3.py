36efb322fcb2c0fbb8dee7b9c9f54c0b557c70b6
Vinicius Perdigao <eng.viniciusperdigao@gmail.com>
2023-01-13 21:03:01 -0300

    Exercício 3

---
+++ Exercício 3.py
@@ -0,0 +1,9 @@
+# Escreva um programa que solicite o nome e o sobrenome do usuário.
+# Ao final o programa deverá apresentar o nome completo do usuário na tela.
+
+nome = input('Digite seu nome: ').strip()
+sobrenome = input('Digite seu nome: ').strip()
+print(f'{nome} {sobrenome}')
+
+
+
