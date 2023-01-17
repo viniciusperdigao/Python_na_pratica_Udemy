6d2dd16dbe9fcbcbe2631159a6d5545f7125a0b8
Vinicius Perdigao <eng.viniciusperdigao@gmail.com>
2023-01-17 08:10:08 -0300

    Exercício 14

---
+++ Exercicio 14 .py
@@ -0,0 +1,11 @@
+# Escreva um programa que peça o nome e a idade do usuário. 
+# Caso a idade do usuário seja maior ou igual a 18 anos apresente a seguinte mensagem: "Seja bem-vindo ao nosso site [nome]!"; 
+# caso contrário, apresente a seguinte mensagem: "Você não pode acessar nosso site [nome].".
+
+nome = input('Digite seu nome: ').strip().title()
+idade = int(input('Digite a sua idade: '))
+
+if idade >= 18:
+    print(f"Seja bem-vindo ao nosso site {nome}!")
+else:
+    print(f"Você não pode acessar nosso site {nome}.")
