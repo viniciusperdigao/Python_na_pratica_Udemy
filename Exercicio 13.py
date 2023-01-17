db553f9ff5361228f3f2236e1a2e3eac2e48bf8b
Vinicius Perdigao <eng.viniciusperdigao@gmail.com>
2023-01-17 08:07:49 -0300

    Exercicio 13

---
+++ Exercicio 13.py
@@ -0,0 +1,5 @@
+# Escreva um programa que peça um número do usuário via método input e converta esse número para o formato float.
+
+num=input('Digite um número: ')
+num=float(num)
+print(type(num))
